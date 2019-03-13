0d6653a93e37a7fd63013a044762982ca0bd60f8
qinjiao <qinjiao@myweimai.com>
2019-03-13 18:25:02 +0800

    upload to oss

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -20,3 +20,9 @@ git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
 
 # Github Pages
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
+
+cd ../public
+
+./ossutil64 config -e oss-cn-hangzhou.aliyuncs.com -i LTAILxlNnWg41hEp -k tkudYbKYLHivTLU59nCOUKMgXZDzx6
+
+./ossutil64 cp public oss://keji-image/ -r -f
