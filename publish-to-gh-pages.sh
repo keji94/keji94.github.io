e790366b8a459d7272a5c3305f6e295d73bf85cf
keji <798631828@qq.com>
2019-03-13 10:53:20 +0800

    update

---
+++ publish-to-gh-pages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ev
+
+# get clone master
+git clone https://${GH_REF} .deploy_git
+cd .deploy_git
+git checkout master
+
+cd ../
+mv .deploy_git/.git/ ./public/
+
+cd ./public
+
+git config user.name "keji"
+git config user.email "798631828@qq.com"
+
+# add commit timestamp
+git add .
+git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
+
+# Github Pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
